176fc9d99ee01686850847893170dc920745c487
Altrrx <Altairmaglianesi@hotmail.com>
2024-03-22 15:59:20 -0300

    Become CMD+ (not start cmd+)

--- Bromas y trucos batch/CMD++ l  CMD+/start_cmd+.bat
+++ Bromas y trucos batch/CMD++ l  CMD+/start_cmd+.bat
@@ -1,34 +1,5 @@
 @echo off
 color 17
-title Launcher De CMD+
+title Launcher CMD+ [Official] v2024.1
 cls
-echo Launcher CMD+ OFICIAL v0.13
-pause
-
-start CMD++
-color 01
-cls
-echo Iniciado.
-pause
-cls
-echo Cerrar?
-pause
-pause
-pause
-pause
-pause
-goto :secreto
-
-:secreto
-color 0C
-cls
-echo BUEN CMD
-color 0A
-cls
-echo BUEN CMD
-color 0B
-cls
-echo BUEN CMD
-goto :secreto
-
-
+call cmd+
